2c24de166366c93dc6fe9a7fae67c0fe85bacba7
root <root@farhangico.openstacklocal>
2026-09-01 15:40:40 +0330

    production next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,8 @@
 const nextConfig = {
     images: {
         dangerouslyAllowLocalIP: true,
-        domains: [
-            'localhost',
-            '192.168.1.220',
-            'api.minnsdev.ir',
-            'api.farhangistore.com',
-            'api.devprojects.ir'
-        ],
         remotePatterns: [
-            { protocol: 'http', hostname: 'localhost', port: '3939', pathname: '/storage/**' },
+            { protocol: 'https', hostname: 'api.minnsdev.ir', pathname: '/storage/**' },
         ],
     }
 };
